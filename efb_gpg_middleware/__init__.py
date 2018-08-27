74f8c6b5eed90d75967fc2c4434320a94c75d581
Zhang Zhishan <zhizha@microsoft.com>
2018-08-27 17:57:28 +0800

    first commit

---
+++ efb_gpg_middleware/__init__.py
@@ -0,0 +1,65 @@
+# coding=utf-8
+
+import os
+import pickle
+import logging
+import uuid
+from pkg_resources import resource_filename
+from gettext import translation
+from typing import Optional, Dict, Tuple
+
+from ehforwarderbot import EFBMiddleware, EFBMsg, utils, MsgType, EFBChat, ChatType, coordinator
+from ehforwarderbot.exceptions import EFBException
+import yaml
+
+from .__version__ import __version__ as version
+
+
+class FilterMiddleware(EFBMiddleware):
+    middleware_id: str = "zhangzhishan.filter"
+    middleware_name: str = "Filter Middleware"
+    __version__: str = version
+
+    mappings: Dict[Tuple[str, str], str] = {}
+    chat: EFBChat = None
+
+
+    def __init__(self, instance_id: str = None):
+        super().__init__(instance_id)
+
+        storage_path = utils.get_data_path(self.middleware_id)
+        config_path = utils.get_config_path(self.middleware_id)
+        if not os.path.exists(storage_path):
+            os.makedirs(storage_path)
+        if not os.path.exists(config_path):
+            raise EFBException(self._("Filter middleware is not configured."))
+        else:
+            config = yaml.load(open(config_path))
+
+        self.chat = EFBChat()
+        self.chat.channel_name = self.middleware_name
+        self.chat.channel_id = self.middleware_id
+        self.chat.channel_emoji = "📜"
+        self.chat.chat_uid = "__zhangzhishan.filter__"
+        self.chat.chat_name = self.middleware_name
+        self.chat.chat_type = ChatType.System
+
+        self.logger = logging.getLogger("blueset.filter")
+
+    def process_message(self, message: EFBMsg) -> Optional[EFBMsg]:
+        self.logger.debug("Received message: %s", message)
+        if not message.type == MsgType.Text:
+            return message
+        self.logger.debug("[%s] is a text message.", message.uid)
+        return message
+
+    def reply_message(self, message: EFBMsg, text: str):
+        reply = EFBMsg()
+        reply.text = text
+        reply.chat = coordinator.slaves[message.chat.channel_id].get_chat(message.chat.chat_uid)
+        reply.author = self.chat
+        reply.type = MsgType.Text
+        reply.deliver_to = coordinator.master
+        reply.target = message
+        reply.uid = str(uuid.uuid4())
+        coordinator.send_message(reply)
---
+++ setup.py
@@ -0,0 +1,47 @@
+import sys
+import os
+from setuptools import setup, find_packages
+
+if sys.version_info < (3, 6):
+    raise Exception("Python 3.6 or higher is required. Your version is %s." % sys.version)
+
+version_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                            'efb_filter_middleware/__version__.py')
+
+__version__ = ""
+exec(open(version_path).read())
+
+long_description = open('README.rst').read()
+
+setup(
+    name='efb-filter-middleware',
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    version=__version__,
+    description='Filter middleware for EH Forwarder Bot, filter using white list and black list.',
+    long_description=long_description,
+    author='Zhishan Zhang',
+    author_email='zhangzhishanlo@gmail.com',
+    url='https://github.com/zhangzhishan/efb-filter-middleware',
+    license='GPLv3',
+    include_package_data=True,
+    python_requires='>=3.6',
+    keywords=['ehforwarderbot', 'EH Forwarder Bot', 'EH Forwarder Bot Master Channel',
+              'Filter'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Communications :: Chat",
+        "Topic :: Utilities"
+    ],
+    install_requires=[
+        "ehforwarderbot"
+    ],
+    entry_points={
+        "ehforwarderbot.middleware": "zhangzhishan.filter = efb_filter_middleware:FilterMiddleware"
+    }
+)
